ed80fb9c1748d3666234e9a6c1b3606a4966b75e
Ovalkin <egorvasilevskiy585@gmail.com>
2024-05-15 00:55:14 +0500

    Начало!

---
+++ README.md
@@ -0,0 +1,19 @@
+# Commit
+```
+cp .env.example .env
+```
+```
+composer install
+```
+```
+sudo chown o+r ./storage -R
+```
+```
+sail up -d
+```
+```
+npm install
+```
+```
+npm run dev
+```
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    @vite(['resources/js/app.js', 'resources/css/app.scss'])
+    <title>BootstrapTest</title>
+</head>
+<body>
+<h1>HAHAHAHAHAHAHAH</h1>
+<button class="btn btn-outline-danger">Hui</button>
+</body>
+</html>
